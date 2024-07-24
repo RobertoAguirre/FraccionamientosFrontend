86e6f43163d561d56481f5cef7be2312fe3d8eb0
Dramzii <dramzii23@gmail.com>
2024-07-24 04:42:26 -0600

    lista para la selcción del ID del Fracc y que podamos hacer pruebas, tambien se anadio grid a lista de servicios para que sea responsiva. Falta: Todavia no cambia la lista de Servicios por fracc seleccionado ya que aun no hemos asociado los servicios a cada fracc

--- src/components/ServiciosForm.js
+++ src/components/ServiciosForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 import {
@@ -21,7 +21,13 @@ import {
   Grid,
   styled,
   ListItemButton,
+  Paper,
 } from "@mui/material";
+
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 import { TabContext, TabList, TabPanel } from "@mui/lab";
 import FolderIcon from "@mui/icons-material/Folder";
@@ -39,77 +45,63 @@ function generate(element) {
   );
 }
 
-const Demo = styled("div")(({ theme }) => ({
-  backgroundColor: theme.palette.background.paper,
-}));
-
 const ServiciosForm = () => {
   const [tabValue, setTabValue] = useState("1");
-  const [response, setResponse] = useState([]);
-  const [nombreServicio, setNombreServicio] = useState("");
+  const [response, setResponse] = useState([]); // Hook para manejar la lista de servicios
+  const [SelectionListValue, setSelectionListValue] = useState("");
 
-  const [dense, setDense] = React.useState(false);
-  const [secondary, setSecondary] = React.useState(false);
-  // const [password, setPassword] = useState('');
-  // const navigate = useNavigate(); //hook para navegar entre rutas
-  // const role= 'user'
+  const [fraccIDList, setFraccIDList] = React.useState([]);
+  const [selectedFraccID, setSelectedFraccID] = React.useState();
 
-  const handleChange = (event, newTabValue) => {
-    setTabValue(newTabValue);
+  // Hooks para manejar
+  const handleSelectionChange = (event) => {
+    // Handle selection list change
+    // setSelectionListValue(event.target.value);
+    setSelectedFraccID(fraccIDList[event.target.value]);
   };
 
+  const handleTabChange = (event, newValue) => {
+    // Handle tab change
+    setTabValue(newValue);
+  };
+  // Hooks para navegar entre rutas en el backend
+
+  //
   const handleServicios = async () => {
     try {
+      // usar get con axios para obtener la lista de servicios
       const response = await axios.get("http://localhost:3001/api/servicios");
-
       setResponse(response.data.servicios);
-
-      // alert("Lista de Servicios encontrada con éxito" + response.data);
-      //   navigate("/login");
+    } catch (err) {
+      console.log(err);
+      alert(`Error al visualizar lista de servicios`);
+    }
+  };
+  const handleFraccIDList = async () => {
+    try {
+      // alert("Intentando conexion");
+      // usar get con axios para obtener la lista de servicios
+      const response = await axios.get("http://localhost:3001/api/fracc");
+      setFraccIDList(response.data.fraccionamientos);
+      // alert("Conexion exitosa");
     } catch (err) {
       console.log(err);
       alert(`Error al visualizar lista de servicios`);
     }
   };
 
+  // UseEffect para cargar la lista de servicios al cargar la página
+  useEffect(() => {
+    handleServicios();
+    handleFraccIDList();
+  }, []);
+
   return (
-    <Box component="form" sx={{ mt: 1 }}>
-      {/* <FormGroup row>
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={dense}
-              onChange={(event) => setDense(event.target.checked)}
-            />
-          }
-          label="Enable dense"
-        />
-        <FormControlLabel
-          control={
-            <Checkbox
-              checked={secondary}
-              onChange={(event) => setSecondary(event.target.checked)}
-            />
-          }
-          label="Enable secondary text"
-        />
-      </FormGroup> */}
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h3" component="div">
-        [Nombre del Fraccionamiento]
-      </Typography>
-
-      <Typography
-        variant="h3
-            "
-        color="text.secondary"
-        sx={{ mt: 2, mb: 8 }}
-      >
-        [ID del fraccionamiento]
-      </Typography>
-
+    <Box component="form" sx={{ mt: 0 }}>
+      {/* Inicia Formulario para seleccion de servicios */}
       <TabContext value={tabValue}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <TabList onChange={handleChange} aria-label="lab API tabs example">
+          <TabList onChange={handleTabChange} aria-label="lab API tabs example">
             <Tab
               onClick={handleServicios}
               label="Lista de servicios"
@@ -119,78 +111,86 @@ const ServiciosForm = () => {
           </TabList>
         </Box>
         <TabPanel value="1">
-          <Grid item xs={12} md={6}>
-            {/* Comineza Muestra extraida de MUI para mostrar formate de lista de servicios */}
-            {/* <Demo>
-              <List dense={dense}>
-                {generate(
-                  <ListItem
-                    secondaryAction={
-                      <div>
+          {/* Inicia Formulario para seleccion de fraccionamiento */}
+          <Paper sx={{ p: 2 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} md={6}>
+                <Typography sx={{}} variant="h3" component="div">
+                  {/* [Nombre del Fraccionamiento] */}
+                  {/* {fraccIDList[0].nombreFracc} */}
+                  {selectedFraccID
+                    ? selectedFraccID.nombreFracc
+                    : "[Nombre del Fraccionamiento]"}
+                </Typography>
+
+                <Typography
+                  variant="h3
+            "
+                  color="text.secondary"
+                  sx={{ mt: 2, mb: 8 }}
+                >
+                  {selectedFraccID
+                    ? selectedFraccID._id
+                    : "[ID del Fraccionamiento]"}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} md={6}>
+                <FormControl sx={{ width: "100%" }}>
+                  <InputLabel id="demo-simple-select-label">
+                    Fracc ID
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={fraccIDList}
+                    label="FraccID"
+                    onChange={handleSelectionChange}
+                    onClick={handleFraccIDList}
+                  >
+                    {fraccIDList.map((fraccIDList, index) => (
+                      <MenuItem key={index} value={index}>
+                        {fraccIDList.nombreFracc} - ID: {fraccIDList._id}
+                      </MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+            </Grid>
+          </Paper>
+          {/* Termina Formulario para seleccion de fraccionamiento */}
+
+          <Paper sx={{ p: 2, mt: 2 }}>
+            <List>
+              <Grid container spacing={2}>
+                {/* // array de servicios */}
+                {response.map((servicio) => (
+                  <Grid item xs={12} md={6}>
+                    <ListItemButton key={servicio.idServicio}>
+                      <ListItemAvatar>
+                        <Avatar>
+                          <FolderIcon />
+                        </Avatar>
+                      </ListItemAvatar>
+                      <ListItemText
+                        primary={servicio.nombreServicio}
+                        secondary={"Secondary text"}
+                        // secondary={secondary ? "Secondary text" : null}
+                      />
+
+                      <Box>
                         <IconButton edge="end" aria-label="delete">
                           <EditIcon />
                         </IconButton>
                         <IconButton edge="end" aria-label="delete">
                           <DeleteIcon />
                         </IconButton>
-                      </div>
-                    }
-                  >
-                    <ListItemAvatar>
-                      <Avatar>
-                        <FolderIcon />
-                      </Avatar>
-                    </ListItemAvatar>
-                    <ListItemText
-                      primary="Single-line item"
-                      secondary={secondary ? "Secondary text" : null}
-                    />
-                  </ListItem>
-                )}
-              </List>
-            </Demo> */}
-            {/* Termin Muestra extraida de MUI para mostrar formate de lista de servicios */}
-          </Grid>
-          {/* <ul> */}
-          <List dense="dense">
-            {response.map((servicio) => (
-              <ListItemButton
-                key={servicio.idServicio}
-                secondaryAction={
-                  <div>
-                    <IconButton edge="end" aria-label="delete">
-                      <EditIcon />
-                    </IconButton>
-                    <IconButton edge="end" aria-label="delete">
-                      <DeleteIcon />
-                    </IconButton>
-                  </div>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar>
-                    <FolderIcon />
-                  </Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary={servicio.nombreServicio}
-                  secondary={secondary ? "Secondary text" : null}
-                />
-
-                <Box>
-                  <IconButton edge="end" aria-label="delete">
-                    <EditIcon />
-                  </IconButton>
-                  <IconButton edge="end" aria-label="delete">
-                    <DeleteIcon />
-                  </IconButton>
-                </Box>
-              </ListItemButton>
-
-              // <li>{servicio.nombreServicio}</li>
-            ))}
-          </List>
-          {/* </ul> */}
+                      </Box>
+                    </ListItemButton>
+                  </Grid>
+                ))}
+              </Grid>
+            </List>
+          </Paper>
         </TabPanel>
         <TabPanel value="2">
           <Typography sx={{ mt: 4, mb: 2 }} variant="h3" component="div">
@@ -198,8 +198,8 @@ const ServiciosForm = () => {
           </Typography>
           <RegisterFormServicios></RegisterFormServicios>
         </TabPanel>
-        <TabPanel value="3">Item Three</TabPanel>
       </TabContext>
+      {/* Termina Formulario para seleccion de servicios */}
     </Box>
   );
 };
